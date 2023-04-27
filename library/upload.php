766303a07e4b56108b73103a0953daa2ede423ea
andrewboylan <andreweboylan@gmail.com>
2023-04-27 14:22:32 -0400

    try again

--- library/upload.php
+++ library/upload.php
@@ -2,7 +2,9 @@
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['file'])) {
 	$filename = $_FILES['file']['name'];
-	$location = 'uploaded_files/'.$filename;
+	$fileExt = pathinfo($filename, PATHINFO_EXTENSION);
+	$newFilename = uniqid().'.'.$fileExt;
+	$location = 'uploaded_files/'.$newFilename;
 
 	if (move_uploaded_file($_FILES['file']['tmp_name'], $location)) {
 		http_response_code(200);
