2e3c7b3180c8af18a8f7186ef64e7e3c6aaf78b8
andrewboylan <andreweboylan@gmail.com>
2023-04-27 14:28:13 -0400

    even more simple

--- library/upload.php
+++ library/upload.php
@@ -8,6 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['file'])) {
 
 	if (move_uploaded_file($_FILES['file']['tmp_name'], $location)) {
 		http_response_code(200);
+		header('X-Filename: '.$newFilename);
 		exit;
 	}
 }
