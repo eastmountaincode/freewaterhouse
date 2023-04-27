997f3e4d6f80003710acc7c1b43c93e866c8ff39
andrewboylan <andreweboylan@gmail.com>
2023-04-27 19:34:28 -0400

    w3 tut

--- library/upload.php
+++ library/upload.php
@@ -1,17 +1,28 @@
 <?php
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['file'])) {
-	$filename = $_FILES['file']['name'];
-	$fileExt = pathinfo($filename, PATHINFO_EXTENSION);
-	$newFilename = uniqid().'.'.$fileExt;
-	$location = 'uploaded_files/'.$newFilename;
-
-	if (move_uploaded_file($_FILES['file']['tmp_name'], $location)) {
-		http_response_code(200);
-		header('X-Filename: '.$newFilename);
-		exit;
-	}
+$target_dir = "uploaded_files/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
 }
-
-http_response_code(500);
-exit;
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+  // if everything is ok, try to upload file
+  } else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    } else {
+      echo "Sorry, there was an error uploading your file.";
+    }
+  }
+?>
