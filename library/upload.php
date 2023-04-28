0bb8f20ebe2a6c07f988790061fe63418dfd2543
andrewboylan <andreweboylan@gmail.com>
2023-04-28 18:34:27 -0400

    accept all filetypes

--- library/upload.php
+++ library/upload.php
@@ -1,22 +1,8 @@
 <?php
-var_dump($_FILES);
+//var_dump($_FILES);
 $target_dir = "uploaded_files/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-
-// $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// // Check if image file is a actual image or fake image
-// if(isset($_POST["submit"])) {
-//   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-//   if($check !== false) {
-//     echo "File is an image - " . $check["mime"] . ".\n";
-//     echo "Target file: " . $target_file . "\n";
-//     $uploadOk = 1;
-//   } else {
-//     echo "File is not an image.";
-//     $uploadOk = 0;
-//   }
-// }
 
 // Check if file is uploaded and has no errors
 if (isset($_POST["submit"]) && $_FILES["fileToUpload"]["error"] === 0) {
