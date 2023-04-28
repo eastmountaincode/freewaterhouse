c37c5c071abb67a9725311f22fb8d4f0979d9044
andrewboylan <andreweboylan@gmail.com>
2023-04-27 20:09:34 -0400

    get php working

--- library/upload.php
+++ library/upload.php
@@ -1,4 +1,5 @@
 <?php
+var_dump($_FILES);
 $target_dir = "/uploaded_files/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -8,6 +9,7 @@ if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
     echo "File is an image - " . $check["mime"] . ".\n";
+    echo "Target file: " . $target_file . "\n";
     $uploadOk = 1;
   } else {
     echo "File is not an image.";
@@ -22,6 +24,7 @@ if ($uploadOk == 0) {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
       echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
     } else {
+      echo "Target file: " . $target_file . "\n";
       echo "Sorry, there was an error uploading your file.";
     }
   }
