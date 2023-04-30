9b6763eeeaf32531b481bd15f2211520565e0c89
andrewboylan <andreweboylan@gmail.com>
2023-04-29 21:05:26 -0400

    added some text in a function

--- library/lending_library.php
+++ library/lending_library.php
@@ -33,6 +33,13 @@
 
     <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     <script src="node_modules/blueimp-file-upload/js/jquery.fileupload.js"></script>
+    <script type="text/javascript">
+        $(function () {
+            $("#fileupload").fileupload({
+                url: 'lending_library.php'
+            });
+        });
+    </script>
 
 </body>
 </html>
