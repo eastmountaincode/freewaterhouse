bce66704cb2dbad5dadef3aabea45befbc4081fa
andrewboylan <andreweboylan@gmail.com>
2023-05-04 12:38:16 -0400

    get jQuery library the right way

--- library/lending_library.php
+++ library/lending_library.php
@@ -51,7 +51,8 @@
 
     <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     <script src="../js/vendor/jquery.ui.widget.js" type="text/javascript"></script>
-    <script src="../js/vendor/jquery.ui.widget.js" type="text/javascript"></script>
+    <script src="../js/vendor/jquery.iframe-transport.js" type="text/javascript"></script>
+    <script src="../js/vendor/jquery.fileupload.js" type="text/javascript"></script>
 </body>
 </html>
 
