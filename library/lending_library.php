0c7b320de286e7ff67a25b1faceacf96bba157fe
andrewboylan <andreweboylan@gmail.com>
2023-05-04 12:21:08 -0400

    get jQuery library the right way

--- library/lending_library.php
+++ library/lending_library.php
@@ -32,8 +32,6 @@
         <h1 id="progress1"></h1>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
-    <script src="node_modules/blueimp-file-upload/js/jquery.fileupload.js"></script>
     <script type="text/javascript">
         $(function () {
             $("#fileupload").fileupload({
@@ -50,6 +48,7 @@
         });
     </script>
 
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
 </body>
 </html>
 
