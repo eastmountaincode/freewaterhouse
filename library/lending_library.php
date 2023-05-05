b417d6a273752b358953426bd2a40c3e23175e72
andrewboylan <andreweboylan@gmail.com>
2023-05-05 15:18:28 -0400

    fix typo

--- library/lending_library.php
+++ library/lending_library.php
@@ -5,7 +5,7 @@
         if (file_exists($targetFile))
             $msg = array("status" => 0, "msg" => "File already exists!");
         else if (move_uploaded_file($_FILES['attachments']['tmp_name'][0], $targetFile))
-            $msg = array("status" => 1, "msg" => "File Has Been Uplaoded", "path" => $targetFile);
+            $msg = array("status" => 1, "msg" => "File Has Been Uploaded", "path" => $targetFile);
         exit(json_encode($msg));
 
     }
