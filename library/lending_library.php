c2fa44dd17d3d2cea0512b0f9bfcd3aaf0d5bbcd
andrewboylan <andreweboylan@gmail.com>
2023-05-05 15:55:05 -0400

    remove previous

--- library/lending_library.php
+++ library/lending_library.php
@@ -12,23 +12,6 @@
 </head>
 <body>
     <h1>Virtual Lending Library</h1>
-    <!-- <form action="upload.php" method="post" enctype="multipart/form-data">
-        <?php
-        $target_dir = "uploaded_files/box1/";
-        $files = array_diff(scandir($target_dir), array('.', '..', '.gitkeep'));
-        
-        if (!empty($files)) {
-            $filename = array_pop($files); // Get the last file in the array (you can change this logic if needed)
-            echo "<p>Current file in the box:</p>";
-            echo "<p><a href=\"" . $target_dir . htmlspecialchars($filename) . "\">" . htmlspecialchars($filename) . "</a></p>";
-        } else {
-            echo "<p>Box is empty.</p>";
-            echo "<input type=\"file\" name=\"fileToUpload\" id=\"fileToUpload\">";
-            echo "<input type=\"submit\" value=\"Upload File\" name=\"submit\">";
-        }
-        ?>
-    </form> -->
-    <br>
     <br>
     <h1>New one</h1>
     <div id = "uploadArea">
