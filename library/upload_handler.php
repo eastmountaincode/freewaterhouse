6ff6de59af4104c6fe4c9f146178d63c0229b4e8
andrewboylan <andreweboylan@gmail.com>
2023-05-07 11:11:58 -0400

    different function

--- library/upload_handler.php
+++ library/upload_handler.php
@@ -7,12 +7,17 @@
         $filename = basename($_FILES['attachments']['name'][0]);
         $targetFile = $upload_directory . $filename;
 
-        if (file_exists($targetFile))
+        if (file_exists($targetFile)) {
             $msg = array("status" => 0, "msg" => "File already exists!");
-        else if (move_uploaded_file($_FILES['attachments']['tmp_name'][0], $targetFile))
-            $msg = array("status" => 1, "msg" => "File Has Been Uploaded", "path" => $targetFile);
-        else
-            $msg = array("status" => -2, "msg" => "File upload failed", "error" => error_get_last());
+        } else {
+            $tmp_file_contents = file_get_contents($_FILES['attachments']['tmp_name'][0]);
+
+            if (file_put_contents($targetFile, $tmp_file_contents) !== false) {
+                $msg = array("status" => 1, "msg" => "File Has Been Uploaded", "path" => $targetFile);
+            } else {
+                $msg = array("status" => -2, "msg" => "File upload failed", "error" => error_get_last());
+            }
+        }
         
         echo json_encode($msg);
         
