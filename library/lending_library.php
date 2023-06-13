f5f0ceb92d7d9be0a2b3de765ddde31a4cc76762
andrewboylan <andreweboylan@gmail.com>
2023-06-12 21:58:21 -0400

    narrow roof

--- library/lending_library.php
+++ library/lending_library.php
@@ -34,7 +34,7 @@
             position: absolute;
             top: 0;
             left: 0;
-            width: 100%;
+            width: 90%;
             height: 0;
             z-index: 1;
         }
