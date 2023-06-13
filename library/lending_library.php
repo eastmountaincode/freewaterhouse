197890436e48426792a6262cbcde2892791b9fb9
andrewboylan <andreweboylan@gmail.com>
2023-06-13 10:41:10 -0400

    try to edit triangle

--- library/lending_library.php
+++ library/lending_library.php
@@ -54,7 +54,7 @@
             transform: translateX(-50%);
             border-left: 48vw solid transparent;
             border-right: 48vw solid transparent;
-            border-bottom: 80px solid #7FFF00;
+            border-bottom: 40px solid #7FFF00;
         }
 
         .trunk {
