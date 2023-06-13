dfa447bd573d026098109e2b5cb71a70f7449f20
andrewboylan <andreweboylan@gmail.com>
2023-06-12 22:21:47 -0400

    header font

--- library/lending_library.php
+++ library/lending_library.php
@@ -52,7 +52,7 @@
             transform: translateX(-50%);
             border-left: 50vw solid transparent;
             border-right: 50vw solid transparent;
-            border-bottom: 150px solid gray;
+            border-bottom: 70px solid gray;
         }
 
         .trunk {
