0f2ea3f37c5a19a8c64bfb3f7a429e4436611037
andrewboylan <andreweboylan@gmail.com>
2023-06-25 21:16:29 -0400

    add jquery

--- library/lending_library.php
+++ library/lending_library.php
@@ -132,6 +132,8 @@
     <br>
     <br>
 
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="main.js"></script>
 
 </body>
