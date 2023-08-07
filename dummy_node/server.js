52e2d3ae01ff0cd850010fee258aba1a76a83660
andrewboylan <andreweboylan@gmail.com>
2023-08-07 16:39:27 -0400

    change port to 3001

--- dummy_node/server.js
+++ dummy_node/server.js
@@ -1,7 +1,7 @@
 console.log('top of server page');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
