bc5d7373d10e6d2297179c9992419bbf409edf7d
andrewboylan <andreweboylan@gmail.com>
2023-08-06 20:38:40 -0400

    collage route

--- collage/public/image_logic.js
+++ collage/public/image_logic.js
@@ -1,5 +1,5 @@
 // Create WebSocket connection.
-const socket = new WebSocket("wss://localhost:3000");
+const socket = new WebSocket("ws://localhost:3000");
     
 // Connection opened
 socket.addEventListener("open", (event) => {
