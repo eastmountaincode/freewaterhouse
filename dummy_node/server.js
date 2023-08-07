498dc026131501a0a3f16051082d7c9e31825eb0
andrewboylan <andreweboylan@gmail.com>
2023-08-07 17:10:19 -0400

    set up web socket

--- dummy_node/server.js
+++ dummy_node/server.js
@@ -1,5 +1,7 @@
 console.log('top of server page');
 const express = require('express');
+const http = require('http'); // This was missing.
+const WebSocket = require('ws');
 const app = express();
 const port = 3001;
 var path = require('path');
@@ -8,6 +10,20 @@ app.get('/dummy_node', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'dummy.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Dummynode listening at http://localhost:${port}`);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    console.log(`Received message => ${message}`);
+  });
+
+  ws.send('Hello! Message from server.');
 });
+
+server.listen(port, () => {
+  console.log(`Dummynode and WebSocket listening at http://localhost:${port}`);
+});
+
