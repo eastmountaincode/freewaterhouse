41187f330b8e01add2bfece5530572bc7ad8b78f
andrewboylan <andreweboylan@gmail.com>
2023-08-14 19:34:10 -0400

    try throttle again

--- collage/public/image_logic.js
+++ collage/public/image_logic.js
@@ -39,8 +39,7 @@ socket.addEventListener('error', (error) => {
     console.error('WebSocket Error:', error);
 });
 
-let updateTimer; // A variable to hold the timer for throttling
-const throttleDelay = 100; // Delay of 100 milliseconds
+let shouldSendUpdate = true;
 
 document.addEventListener("DOMContentLoaded", function() { 
     // Make all images inside the #imageArea draggable
@@ -67,16 +66,18 @@ document.addEventListener("DOMContentLoaded", function() {
             event.target.setAttribute("data-x", x);
             event.target.setAttribute("data-y", y);
 
-            // Throttle the position update messages
-            clearTimeout(updateTimer); // Clear any existing timer
-            updateTimer = setTimeout(() => {
+            if (shouldSendUpdate) {
                 socket.send(JSON.stringify({
                     type: 'updatePositionOnSocketDragging',
                     id: event.target.id,
                     x: x,
                     y: y  
                 }));
-            }, throttleDelay);
+                shouldSendUpdate = false;
+                setTimeout(() => {
+                    shouldSendUpdate = true;
+                }, throttleDelay);
+            }
 
             // socket.send(JSON.stringify({
             //   type: 'updatePositionOnSocketDragging',
@@ -86,8 +87,6 @@ document.addEventListener("DOMContentLoaded", function() {
             // }));
           },
           end(event) {
-            // Ensure the final position is sent immediately when dragging ends
-            clearTimeout(updateTimer);
 
             const target = event.target;
             const id = target.id;
@@ -104,103 +103,3 @@ document.addEventListener("DOMContentLoaded", function() {
         }
       });
 });
-
-// // Make the image draggable
-// const images = document.querySelectorAll("img");
-// const imageArea = document.querySelector("#imageArea");
-// const imageAreaRect = imageArea.getBoundingClientRect();
-
-// let mouseX = 0;
-// let mouseY = 0;
-// let imgOffsetX = 0;
-// let imgOffsetY = 0;
-// let isDragging = false;
-// let draggedImage = null;
-
-// images.forEach(image => {
-//     image.addEventListener("mousedown", startDrag);
-//     image.addEventListener("touchstart", startDrag);
-// });
-
-// document.addEventListener("mouseup", endDrag);
-// document.addEventListener("touchend", endDrag);
-
-// document.addEventListener("mousemove", drag);
-// document.addEventListener("touchmove", drag);
-
-// function startDrag(event) {
-//     event.preventDefault();
-//     if (event.type === 'touchstart') {
-//         event = event.touches[0];  // get the first touch event
-//     }
-
-//     isDragging = true;
-//     draggedImage = event.target;  // Store the dragged image
-//     imgOffsetX = draggedImage.offsetLeft - event.clientX;
-//     imgOffsetY = draggedImage.offsetTop - event.clientY;
-// }
-
-// function endDrag() {
-//     if (draggedImage) {
-//         socket.send(JSON.stringify({
-//             type: 'updatePositionInDatabase',
-//             id: draggedImage.id,
-//             x: confirmedNewX,
-//             y: confirmedNewY
-//         }));
-//         isDragging = false;
-//         draggedImage = null;  // Clear the dragged image
-//     }
-// }
-
-// function drag(event) {
-//     if (event.type === 'touchmove') {
-//         event = event.touches[0];  // get the first touch event
-//     }
-
-//     if (isDragging && draggedImage) {
-//         // Position of the mouse pointer within the browser's viewport
-//         mouseX = event.clientX;
-//         mouseY = event.clientY;
-
-//         let prospectiveNewX = mouseX + imgOffsetX;
-//         let prospectiveNewY = mouseY + imgOffsetY;
-
-//         //let imageAreaRect = imageArea.getBoundingClientRect();
-//         let draggedImageRect = draggedImage.getBoundingClientRect();
-
-//         let maxPosX = imageAreaRect.width - draggedImageRect.width;
-//         let maxPosY = imageAreaRect.height - draggedImageRect.height;
-
-//         // LEFT WALL
-//         if (prospectiveNewX < 0) {
-//             prospectiveNewX = 0;
-//         }
-//         // TOP WALL
-//         if (prospectiveNewY < 0) {
-//             prospectiveNewY = 0;
-//         }
-//         // RIGHT WALL
-//         if (prospectiveNewX > maxPosX) {
-//             prospectiveNewX = maxPosX;
-//         }
-//         // BOTTOM WALL
-//         if (prospectiveNewY > maxPosY) {
-//             prospectiveNewY = maxPosY;
-//         }
-
-//         confirmedNewX = prospectiveNewX;
-//         confirmedNewY = prospectiveNewY;
-
-//         draggedImage.style.left = confirmedNewX + 'px';
-//         draggedImage.style.top = confirmedNewY + 'px';
-
-//         socket.send(JSON.stringify({
-//             type: 'updatePositionOnSocketDragging',
-//             id: draggedImage.id,  // include the ID in the message
-//             x: confirmedNewX,
-//             y: confirmedNewY  
-            
-//         }));
-//     }
-// }
