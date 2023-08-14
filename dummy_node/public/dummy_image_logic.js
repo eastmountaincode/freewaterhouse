3877f43915d1e3f2531a5bf557c3f7cad0074c91
andrewboylan <andreweboylan@gmail.com>
2023-08-13 21:21:59 -0400

    fleshing out dummy page

--- dummy_node/public/dummy_image_logic.js
+++ dummy_node/public/dummy_image_logic.js
@@ -3,6 +3,6 @@ const socket = new WebSocket('wss://freewaterhouse.com/ws2');
  
 // Connection opened
 socket.addEventListener("open", (event) => {
-    console.log("Connected to websocket server");
+    console.log("Connected to websocket server dummy time");
 
 });
