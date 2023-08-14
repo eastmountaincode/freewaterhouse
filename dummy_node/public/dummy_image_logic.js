28689e1b1c8bc773009c78216f567b757d69c040
andrewboylan <andreweboylan@gmail.com>
2023-08-13 21:31:31 -0400

    add drag logic

--- dummy_node/public/dummy_image_logic.js
+++ dummy_node/public/dummy_image_logic.js
@@ -6,3 +6,32 @@ socket.addEventListener("open", (event) => {
     console.log("Connected to websocket server dummy time");
 
 });
+
+document.addEventListener("DOMContentLoaded", function() { // Make sure the DOM is fully loaded before attaching handlers.
+
+    // Make all images inside the #imageArea draggable
+    interact("#imageArea img")  // Selects all img elements inside the #imageArea
+      .draggable({
+        inertia: true, 
+        restrict: { 
+          restriction: "parent",
+          endOnly: true,
+        },
+        autoScroll: true,
+  
+        // Event listeners for dragmove and dragend
+        listeners: {
+          move(event) {
+            var x = (parseFloat(event.target.getAttribute("data-x")) || 0) + event.dx;
+            var y = (parseFloat(event.target.getAttribute("data-y")) || 0) + event.dy;
+  
+            // Translate the element
+            event.target.style.transform = "translate(" + x + "px, " + y + "px)";
+  
+            // Update the position attributes
+            event.target.setAttribute("data-x", x);
+            event.target.setAttribute("data-y", y);
+          }
+        }
+      });
+  });
