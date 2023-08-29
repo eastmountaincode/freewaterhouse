2232fc5081d20048c1db640ee4f77e63bb6b49d2
andrewboylan <andreweboylan@gmail.com>
2023-08-29 11:43:27 -0400

    add how to section

--- collage/public/image_logic.js
+++ collage/public/image_logic.js
@@ -675,4 +675,18 @@ closeModal.addEventListener('click', function() {
     modal.style.display = "none";
 });
 
+document.getElementById("toggleButton").addEventListener("click", function(e) {
+    e.preventDefault(); 
+    var moreText = document.getElementById("more");
+    var button = document.getElementById("toggleButton");
+    var computedStyle = window.getComputedStyle(moreText);
+    if (computedStyle.display === "none") {
+        moreText.style.display = "block";
+        button.innerText = "About⬆";
+    } else {
+        moreText.style.display = "none";
+        button.innerText = "About⬇";
+    }
+});
+
 
